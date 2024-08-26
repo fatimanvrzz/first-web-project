af981bf973b7e51715581ed5a5dd6042192fe551
nullshii <no>
2024-08-26 15:15:19 +0400

    Shitload of fixes after fatima

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,8 +13,37 @@ type PricingData = {
     Price: number;
 }
 
+type AppointmentData = {
+    Placeholder: string;
+    IconUrl: string;
+    IconAltText: string;
+}
+
 const IndexPage: React.FC = () => {
     const [activeServiceIndex, serActiveServiceIndex] = useState(0);
+
+    const appointments: AppointmentData[] = [
+        {
+            Placeholder: "Choose Your Location",
+            IconUrl: "https://zcube.in/hitup/img/bg/ad-icon1.png",
+            IconAltText: "LocationIcon",
+        },
+        {
+            Placeholder: "Choose Visiting Date",
+            IconUrl: "https://zcube.in/hitup/img/bg/ad-icon2.png",
+            IconAltText: "DateIcon",
+        },
+        {
+            Placeholder: "Choose Your Gym",
+            IconUrl: "https://zcube.in/hitup/img/bg/ad-icon3.png",
+            IconAltText: "DumbellIcon",
+        },
+        {
+            Placeholder: "Choose Your Gym Coach",
+            IconUrl: "https://zcube.in/hitup/img/bg/ad-icon4.png",
+            IconAltText: "PersonIcon",
+        },
+    ];
 
     const services: ServiceData[] = [
         {
@@ -79,39 +108,43 @@ const IndexPage: React.FC = () => {
             ],
             Price: 149.99,
         },
-
     ];
 
     return (
         <div className="flex flex-col">
             <div className="flex flex-col">
-                <div className="bg-purple h-lvh w-screen text-justify">
-                    <h2 className="flex flex-col w-64 font-extrabold text-white font-noto-sans text-4xl mt-20 ml-10 leading-tight">
+                <div className="bg-purple h-[40rem] w-screen text-justify">
+                    <h2 className="flex flex-col max-w-64 font-extrabold text-white font-noto-sans text-4xl mt-20 ml-10 leading-tight">
                         Perfect Workout Coach For
                         <span>
                             Body Shape
                         </span>
                     </h2>
-                    <p className="flex w-64 mt-9 ml-10 text-white font-noto-sans ">Quisque leo augue, lobortis ac tellus nec, posuere ultricies nulla. Praesent massa odio, pellentesque in consectetur quis, volutpat sit amet erat.</p>
+                    <p className="flex max-w-64 mt-9 ml-10 text-white font-noto-sans ">Quisque leo augue, lobortis ac tellus nec, posuere ultricies nulla. Praesent massa odio, pellentesque in consectetur quis, volutpat sit amet erat.</p>
                     <div className="flex border-2 border-secondary bg-secondary text-white w-fit ml-10 mt-9 rounded-xl px-7 py-3 shadow-secondary shadow-all-around">
                         <p className="font-noto-sans tracking-wider text-sm">Learn More</p>
                     </div>
                 </div>
             </div>
-            <div className="flex flex-col self-center h-96 w-64 pb-5 rounded-3xl bg-white -translate-y-10 shadow-all-around shadow-neutral-300">
-                <div className="flex flex-col items-center justify-around h-full">
-                    <AppointmentBookElement iconLink="https://zcube.in/hitup/img/bg/ad-icon1.png" iconAltText="LocationIcon" placeholder="Choose Your Location" />
-                    <AppointmentBookElement iconLink="https://zcube.in/hitup/img/bg/ad-icon2.png" iconAltText="LocationIcon" placeholder="Choose Visiting Date" />
-                    <AppointmentBookElement iconLink="https://zcube.in/hitup/img/bg/ad-icon3.png" iconAltText="LocationIcon" placeholder="Choose Your Gym" />
-                    <AppointmentBookElement iconLink="https://zcube.in/hitup/img/bg/ad-icon4.png" iconAltText="LocationIcon" placeholder="Choose Your Gym Coach" />
+            <div className="flex w-screen items-center justify-center px-6">
+                <div className="flex flex-col self-center w-full h-fit py-10 px-6 gap-5 rounded-xl bg-white -translate-y-10 shadow-all-around shadow-neutral-800/20">
+                    <div className="flex flex-col items-start px-2 gap-10">
+                        {appointments.map((appointment) => {
+                            return (
+                                <AppointmentBookElement data={appointment} />
+                            )
+                        })}
+                    </div>
+                    <button className="w-full self-center text-white bg-secondary rounded-xl h-20 font-semibold font-noto-sans">
+                        Book Your Appointment
+                    </button>
                 </div>
-                <button className="w-max self-center mx-4 px-3 text-white bg-secondary rounded-xl h-20 font-semibold font-noto-sans">
-                    Book Your Appointment
-                </button>
             </div>
             <div className="flex flex-col">
-                <img src="https://zcube.in/hitup/img/bg/about-img.png" alt="" className=" w-64 mt-20 mb-7" />
-                <h2 className="w-60 font-noto-sans text-2xl mt-10 ml-6 font-extrabold text-pretty">Let Us Find The Perfect Workout For You !</h2>
+                <div className=" mt-20 mb-7 self-center">
+                    <img src="https://zcube.in/hitup/img/bg/about-img.png" alt="" className="w-full aspect-auto -ml-[55px]" />
+                </div>
+                <h2 className="font-noto-sans text-2xl mt-10 ml-6 font-extrabold text-pretty self-center">Let Us Find The Perfect Workout For You !</h2>
                 <img src="https://zcube.in/hitup/img/bg/circle-line.png" alt="" className="w-fit h-fit ml-6 mt-3 mb-9" />
                 <div className="flex flex-col gap-10 w-screen px-6">
                     <span className="text-pretty text-grey">Fusce orci ligula, tincidunt ut metus vel, venenatis aliquet tortor. Duis et consequat enim. Curabitur pulvinar, dolor at pulvinar molestie, augue massa volutpat felis, at rhoncus tortor velit vel diam. Cras ac suscipit metus.</span>
@@ -125,7 +158,7 @@ const IndexPage: React.FC = () => {
                 </div>
             </div>
             <div className="bg-slate-50 w-screen">
-                <h2 className="w-64 text-2xl font-noto-sans font-extrabold ml-6 mt-28">Build Your Body Transform Your Life</h2>
+                <h2 className="text-2xl font-noto-sans font-extrabold ml-6 mt-28">Build Your Body Transform Your Life</h2>
                 <img className="w-fit aspect-auto ml-6 mt-3 mb-9" src="https://zcube.in/hitup/img/bg/circle-line.png" alt="" />
                 <div className="flex flex-col gap-5 w-screen px-6">
                     <span className="text-pretty text-grey">Integer auctor est nec semper hendrerit. Etiam sollicitudin enim in urna commodo, in dapibus velit elementum. Ut congue nisl sapien, vel ultricies nulla tincidunt vitae. Maecenas non ligula quis massa consequat commodo eleifend ut velit.</span>
@@ -147,7 +180,7 @@ const IndexPage: React.FC = () => {
                     <img className="w-fit aspect-auto ml-10 mt-3 mb-9" src="https://zcube.in/hitup/img/bg/circle-line-w.png" alt="" />
                     <p className="flex w-64 mt-9 ml-10 text-white font-noto-sans ">Integer auctor est nec semper hendrerit. Etiam sollicitudin enim in urna commodo, in dapibus velit elementum. Ut congue nisl sapien, vel ultricies nulla tincidunt vitae. Maecenas non ligula quis massa consequat commodo eleifend ut velit.</p>
                     <p className="flex w-64 mt-4 ml-10 text-white font-noto-sans ">Aenean eleifend est eget eros molestie pretium. Curabitur varius velit in est elementum consectetur. Mauris in risus bibendum, laoreet ex at, lobortis risus. Nullam id massa eget ante finibus iaculis. Duis eget nisl ipsum. Vivamus ipsum urna, tristique vel arcu nec, elementum congue est. Nullam vitae aliquet turpis.</p>
-                    <div className="flex items-center justify-center bg-[url(https://zcube.in/hitup/img/bg/video-img.png)] mt-5 ml-10 h-44 bg-cover">
+                    <div className="flex items-center justify-center bg-[url(https://zcube.in/hitup/img/bg/video-img.png)] mt-5 ml-10 h-60 bg-cover">
                         <img src="https://zcube.in/hitup/img/bg/play-icon.png" alt="play-button" className="aspect-auto h-16" />
                     </div>
                 </div>
@@ -163,11 +196,9 @@ const IndexPage: React.FC = () => {
                     })}
                 </div>
             </div>
-            <div className="bg-slate-50 w-screen mt-36">
-                <h2 className="w-64 text-3xl text-center font-noto-sans font-bold ml-6 mt-28">What Our Client Say’s</h2>
-                <img className="w-fit aspect-auto ml-28 mt-3 mb-9" src="https://zcube.in/hitup/img/bg/circle-line.png" alt="" />
-                
-                
+            <div className="bg-slate-50 w-screen mt-36 flex flex-col items-center">
+                <h2 className="text-3xl text-center font-noto-sans font-bold ml-6 mt-28">What Our Client Say’s</h2>
+                <img className="w-fit aspect-auto mt-3 mb-9" src="https://zcube.in/hitup/img/bg/circle-line.png" alt="" />
             </div>
         </div>
     );
@@ -176,11 +207,11 @@ const IndexPage: React.FC = () => {
 export default IndexPage;
 
 
-const AppointmentBookElement: React.FC<{ iconLink: string, iconAltText?: string, placeholder?: string }> = (props) => {
+const AppointmentBookElement: React.FC<{ data: AppointmentData }> = (props) => {
     return (
-        <div className="flex flex-row gap-3 items-center text-sm">
-            <img src={props.iconLink} alt={props.iconAltText} className="aspect-auto h-fit" />
-            <input type="text" name={props.iconAltText} placeholder={props.placeholder} className="outline-none" />
+        <div className="flex flex-row gap-3 items-center text-sm h-16 justify-center">
+            <img src={props.data.IconUrl} alt={props.data.IconAltText} className="aspect-auto h-fit" />
+            <input type="text" placeholder={props.data.Placeholder} className="outline-none max-w-fit" />
         </div>
     )
 }
@@ -207,7 +238,8 @@ const ServiceItem: React.FC<{ isActive?: boolean, onClick?: React.MouseEventHand
 
 const PricingItem: React.FC<{ data: PricingData }> = (props) => {
     return (
-        <div className="flex flex-col w-full h-[649px] items-center bg-[url(https://zcube.in/hitup/img/bg/price-bg.png)] bg-cover border shadow-neutral-300 shadow-2xl overflow-hidden rounded-2xl">
+        <div className="flex flex-col w-full h-[649px] items-center bg-[url(https://zcube.in/hitup/img/bg/price-bg.png)] bg-cover border
+            shadow-neutral-300 shadow-2xl overflow-hidden rounded-2xl">
             <h1 className="text-center font-bold text-2xl text-white mt-10">
                 {props.data.Title}
             </h1>
